fc1c7270dee1d6c82e641768c16a884c4b10a72b
Chris <brophy.c03@gmail.com>
2023-11-18 16:38:45 +0000

    Added mini-interpreters.

--- Department.java
+++ Department.java
@@ -2,17 +2,37 @@ import java.util.Scanner;
 
 public class Department {
     private String name;
+    private Scanner in;
 
     public Department(String _name) {
         this.name = _name;
+        in = new Scanner(System.in);
     }
 
     public void run() {
-        Scanner in = new Scanner(System.in);
-    }
-
-    public String getName() {
-        return this.name;
+        System.out.printf("[Faculty of %s]\n", this.name.toUpperCase());
+        boolean run = true;
+        while (run) {
+            System.out.println("Commands: (W)rite Message, (H)old Exam Board, (E)xit");
+            String command = in.nextLine().toUpperCase();
+            switch (command) {
+                case "W":
+                    System.out.println("What is the student's ID?");
+                    String id = in.nextLine();
+                    System.out.println("What is the message?");
+                    String message = in.nextLine();
+                    this.writeMessageToStudent(id, message);
+                    break;
+                case "H":
+                    this.holdExamBoard();
+                    break;
+                case "E":
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Invalid command.");
+            }
+        }
     }
 
     public boolean holdExamBoard() {
@@ -20,6 +40,6 @@ public class Department {
     }
 
     public void writeMessageToStudent(String id, String message) {
-
+        CSVWriter.writeStudentMessage(id, message);
     }
 }
--- Student.java
+++ Student.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Student {
     private String id;
+    private Scanner in;
 
     public Student(String _id) {
         this.id = _id;
+        in = new Scanner(System.in);
     }
 
     public void run() {
-
+        System.out.printf("[Student ID: %s]\n", this.id);
+        boolean run = true;
+        while (run) {
+            System.out.println("Commands: (G)et transcript, (E)xit");
+            String command = in.nextLine().toUpperCase();
+            switch (command) {
+                case "G":
+                    this.getTranscript();
+                    break;
+                case "E":
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Invalid command.");
+            }
+        }
     }
 
     public void getTranscript() {
