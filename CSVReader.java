592a07446204a00299879b5782f60b730a2e5751
Chris <brophy.c03@gmail.com>
2023-11-18 14:40:10 +0000

    Wrote signatures for methods in CSVReader.java and CSVWriter.java.

---
+++ CSVReader.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CSVReader {
+    public static void readGradingScheme(String fileName, int[] bounds, String[] grades) {
+        return;
+    }
+
+    public static ArrayList<Student> readClassRole(String fileName) {
+        ArrayList<Student> students = new ArrayList<Student>();
+
+        return students;
+    }
+
+    public static ArrayList<Student> readStudentMessages(String id) {
+        ArrayList<Student> messages = new ArrayList<Student>();
+
+        return messages;
+    }
+}
---
+++ CSVWriter.java
@@ -0,0 +1,13 @@
+public class CSVWriter {
+    public static void insertGrade(String id, double grade, String moduleName) {
+
+    }
+
+    public static void removeGrade(String id, int index, String moduleName) {
+
+    }
+
+    public static void writeStudentMessage(String message) {
+
+    }
+}
