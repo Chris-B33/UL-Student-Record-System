5b499fc7646956bb39f621cdddc9693545a35892
daraDom <Dara.omalley.dom@gmail.com>
2023-11-20 14:52:13 +0000

    Modified CSVReader.java and CSVWriter.java

--- CSVReader.java
+++ CSVReader.java
@@ -1,19 +1,86 @@
 import java.util.ArrayList;
+import java.io.*;
 
-public class CSVReader {
-    public static void readGradingScheme(String fileName, int[] bounds, String[] grades) {
-        return;
+public class CSVReader
+{
+    private static String messagesFileName;// = path to file must have done for message system to work.
+    
+    public static void readGradingScheme(String fileName, int[] bounds, String[] grades) throws IOException
+    {
+        String line = "";
+        String splitBy = ",";
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
+        {
+            int i = 0;
+            while ((line = br.readLine()) != null)
+            {
+                String[] gradeScheme = line.split(splitBy);
+                
+                bounds[i] = Integer.parseInt(gradeScheme[0]);
+                grades[i] = gradeScheme[1];
+                
+                i++;
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
-
-    public static ArrayList<Student> readClassRole(String fileName) {
-        ArrayList<Student> students = new ArrayList<Student>();
-
-        return students;
+    
+    public static ArrayList<Student> readClassRole(String fileName)
+    {
+        ArrayList<Student> classRole = new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
+        {
+            String line = "";
+            String splitBy = ",";
+            while ((line = br.readLine()) != null)
+            {
+                String[] roleData = line.split(splitBy);
+                String id = roleData[0];
+                
+                Student s = new Student(id);
+                
+                classRole.add(s);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+        return classRole;
     }
-
-    public static ArrayList<Student> readStudentMessages(String id) {
-        ArrayList<Student> messages = new ArrayList<Student>();
-
+    
+    public static ArrayList<String> readStudentMessages(String id)
+    {
+        ArrayList<String> messages = new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(messagesFileName)))
+        {
+            String line = "";
+            String inbox = "";
+            
+            while ((line = br.readLine()) != null)
+            {
+                String[] messageData = line.split(",");
+                String messageId = messageData[0];
+                
+                if (messageId.equals(id))
+                {
+                    for (int i = 1; i < messageData.length; i++)
+                    {
+                        messages.add(messageData[i]);
+                    }
+                }
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
         return messages;
     }
 }
--- CSVWriter.java
+++ CSVWriter.java
@@ -1,13 +1,75 @@
-public class CSVWriter {
-    public static void insertGrade(String id, double grade, String moduleName) {
+import java.io.*;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 
+public class CSVWriter
+{
+    private static String messagesFileName;// = path to file must have done for message system to work.
+    
+    public static void insertGrade(String id, double grade, String moduleName) throws IOException 
+    {
+        File csvFile = new File(moduleName);
+        List<String> lines = Files.readAllLines(csvFile.toPath(), StandardCharsets.UTF_8);
+        
+        for (int i = 0; i < lines.size(); i++)
+        {
+            String[] results = lines.get(i).split(",");
+            if (results[0].equals(id))
+            {
+                lines.set(i,lines.get(i) + "," + grade);
+                break;
+            }
+        }
+        
+        Files.write(csvFile.toPath(), lines, StandardCharsets.UTF_8);
     }
 
-    public static void removeGrade(String id, int index, String moduleName) {
-
+    public static void removeGrade(String id, int index, String moduleName) throws IOException 
+    {
+        Path path = Paths.get(moduleName);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        
+        for (int i = 0; i < lines.size(); i++)
+        {
+            String[] results = lines.get(i).split(",");
+            if (results[0].equals(id))
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < results.length; j++)
+                {
+                    if (j != index)
+                    {
+                        sb.append(results[j]);
+                        if (j != results.length - 1)
+                        {
+                            sb.append(",");
+                        }
+                    }
+                }
+                lines.set(i, sb.toString());
+                break;
+            }
+        }
+        Files.write(path, lines, StandardCharsets.UTF_8);
     }
 
-    public static void writeStudentMessage(String id, String message) {
-
+    public static void writeStudentMessage(String id, String message) throws IOException 
+    {
+        Path path = Paths.get(messagesFileName);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        
+        for (int i = 0; i < lines.size(); i++)
+        {
+            String[] messages = lines.get(i).split(",");
+            if (messages[0].equals(id))
+            {
+                lines.set(i,lines.get(i) + "," + message);
+                break;
+            }
+        }
+        Files.write(path, lines, StandardCharsets.UTF_8);
     }
 }
+
